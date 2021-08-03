c1a5e061c606279a56ad70056822174debf763a6
Reny Grando <46140428+renygrando@users.noreply.github.com>
2021-08-03 08:12:48 -0300

    criando meu primeiro componente

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,14 @@
+export function ListaDeNotas() {
+  return (
+    <ul>
+      <li>
+        <section>
+          <header>
+            <h3>Título</h3>
+          </header>
+          <p>Escreva sua nota</p>
+        </section>
+      </li>
+    </ul>
+  );
+}
