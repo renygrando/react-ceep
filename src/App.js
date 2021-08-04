a5a256b5691687d5050c2dd8fe123f3b18446385
Reny Grando <46140428+renygrando@users.noreply.github.com>
2021-08-04 09:19:30 -0300

    inserindo importação dos assets css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import FormularioCadastro from "./components/FormularioCadastro";
 import ListaDeNotas from "./components/ListaDeNotas";
+import "./assets/index.css";
+import "./assets/App.css";
 
 class App extends Component {
   render() {
