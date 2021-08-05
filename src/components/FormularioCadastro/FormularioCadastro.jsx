b92370d6fd28ed8b6a873e8ecabb44d6e19201f8
Reny Grando <46140428+renygrando@users.noreply.github.com>
2021-08-05 09:24:47 -0300

    aplicando classes nas tags

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
+import "./estilo.css";
+
 class FormularioCadastro extends Component {
   render() {
     return (
-      <form>
-        <input type="text" placeholder="Título" />
-        <textarea placeholder="Escreva sua nota..." />
-        <button>Criar nota</button>
+      <form className="form-cadastro">
+        <input
+          type="text"
+          placeholder="Título"
+          className="form-cadastro_input"
+        />
+        <textarea
+          placeholder="Escreva sua nota..."
+          className="form-cadastro_input"
+          rows="8"
+        />
+        <button className="form-cadastro_input form-cadastro_submit">
+          Criar nota
+        </button>
       </form>
     );
   }
