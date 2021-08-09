a6e75d5722a44553b182e085384cf57b41eee6e5
Reny Grando <46140428+renygrando@users.noreply.github.com>
2021-08-09 10:55:24 -0300

    Criando handleMudancaTitulo e aprendendo bind

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,6 +3,16 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaTitulo(evento) {
+    this.titulo = evento.target.value;
+    console.log(this.titulo);
+  }
+
   render() {
     return (
       <form className="form-cadastro">
@@ -10,6 +20,7 @@ class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           placeholder="Escreva sua nota..."
